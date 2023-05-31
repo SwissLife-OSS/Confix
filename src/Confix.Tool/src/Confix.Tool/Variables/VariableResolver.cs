e962a5bb31b1e393da7d63d8fd8aa0e85c26e595
Florian Rohrer <florian.rohrer@swisslife.ch>
2023-05-31 16:33:34 +0200

    VariableResolver

---
+++ src/Confix.Tool/src/Confix.Tool/Variables/VariableResolver.cs
@@ -0,0 +1,48 @@
+using Confix.Tool;
+
+namespace ConfiX.Variables;
+
+public class VariableResolver
+{
+    private readonly IVariableProviderFactory _variableProviderFactory;
+
+    public VariableResolver(IVariableProviderFactory variableProviderFactory)
+    {
+        _variableProviderFactory = variableProviderFactory;
+    }
+
+    public async Task<IReadOnlyDictionary<VariablePath, string>> ResolveVariables(
+        IReadOnlyList<VariablePath> keys,
+        IReadOnlyList<VariableProviderConfiguration> configurations,
+        CancellationToken cancellationToken)
+    {
+        Dictionary<VariablePath, string> resolvedVariables = new();
+
+        foreach (IGrouping<string, VariablePath> group in keys.GroupBy(k => k.ProviderName))
+        {
+            VariableProviderConfiguration configuration = GetProviderConfiguration(configurations, group.Key);
+            IVariableProvider provider = _variableProviderFactory.CreateProvider(
+                configuration.Type,
+                configuration.Configuration);
+
+            var resolved = await provider.ResolveManyAsync(
+                group.Select(x => x.Path).ToArray(), 
+                cancellationToken);
+
+            resolved.ForEach((r) =>
+                resolvedVariables.Add(
+                    group.First(i => i.Path == r.Key), 
+                    r.Value));
+        }
+
+        return resolvedVariables;
+    }
+
+    private static VariableProviderConfiguration GetProviderConfiguration(
+        IReadOnlyList<VariableProviderConfiguration> configurations,
+        string providerName) 
+        => configurations.FirstOrDefault(c => c.Name.Equals(providerName)) 
+            ?? throw new InvalidOperationException("");
+
+
+}
