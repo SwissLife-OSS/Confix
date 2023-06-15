fcb11b1447737becc5f4172f9880080b9b6fdc5c
Florian Rohrer <dev@florianrohrer.ch>
2023-06-15 08:58:44 +0200

    Implement VariableIntellisense integration

--- src/Confix.Tool/src/Confix.Tool/Entities/Project/ProjectComposer.cs
+++ src/Confix.Tool/src/Confix.Tool/Entities/Project/ProjectComposer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text.Json.Nodes;
 using Confix.Tool.Abstractions;
 using Confix.Tool.Schema;
@@ -9,17 +10,38 @@ namespace Confix.Tool.Entities.Components.DotNet;
 public sealed class ProjectComposer
     : IProjectComposer
 {
+    private static class WellKnown{
+        public const string ConfixVariables = "Confix_Variables";
+    }
     public JsonSchema Compose(IEnumerable<Component> components, IEnumerable<VariablePath> variables)
     {
-        var defs = new Dictionary<string, JsonSchema>();
-        var properties = new Dictionary<string, JsonSchema>();
+        var enumeratedComponents = components.ToArray();
+        var defs = GetPrefixedDefinitions(enumeratedComponents);
+        var properties = GetProperties(enumeratedComponents);
 
-        defs["Variable"] = variables.ToVariableStringType();
+        var variableType = GetVariableType(variables);
 
+        defs.Add(WellKnown.ConfixVariables, variableType);
+
+        return new JsonSchemaBuilder()
+            .Defs(defs)
+            .Properties(properties)
+            .Required(properties.Keys.ToArray())
+            .Build();
+    }
+
+    private static Dictionary<string, JsonSchema> GetPrefixedDefinitions(IEnumerable<Component> components)
+    {
+        Dictionary<string, JsonSchema> defs = new();
         foreach (var componentDefinition in components)
         {
             var prefixedJsonSchema =
-                componentDefinition.Schema.PrefixTypes($"{componentDefinition.ComponentName}_");
+                componentDefinition.Schema
+                    .PrefixTypes($"{componentDefinition.ComponentName}_")
+                    .AddVariableIntellisense(new JsonObject()
+                    {
+                        [RefKeyword.Name] = $"#/$defs/{WellKnown.ConfixVariables}"
+                    });
 
             if (prefixedJsonSchema.GetDefs() is { } prefixedDefs)
             {
@@ -37,30 +59,24 @@ public sealed class ProjectComposer
                 .Examples(prefixedJsonSchema.GetExamples() ?? Array.Empty<JsonNode>())
                 .Title(prefixedJsonSchema.GetTitle() ?? string.Empty)
                 .Build();
+        }
 
+        return defs;
+    }
+
+    private static Dictionary<string, JsonSchema> GetProperties(IEnumerable<Component> components)
+    {
+        Dictionary<string, JsonSchema> properties = new();
+        foreach (var componentDefinition in components)
+        {
             properties[componentDefinition.ComponentName] = new JsonSchemaBuilder()
                 .Ref($"#/$defs/{componentDefinition.ComponentName}")
                 .Build();
         }
-
-        return new JsonSchemaBuilder()
-            .Defs(defs)
-            .Properties(properties)
-            .Required(properties.Keys.ToArray())
-            .Build();
+        return properties;
     }
-}
 
-file static class Extensions
-{
-    public static JsonSchemaBuilder ToVariableStringType(this IEnumerable<VariablePath> variables)
-       => new JsonSchemaBuilder()
-            .AnyOf(
-                variables.ToVariableType(),
-                new JsonSchemaBuilder().Type(SchemaValueType.String)
-            );
-
-    private static JsonSchemaBuilder ToVariableType(this IEnumerable<VariablePath> variables)
+    private static JsonSchemaBuilder GetVariableType(IEnumerable<VariablePath> variables)
         => new JsonSchemaBuilder()
             .Type(SchemaValueType.String)
             .Enum(variables.Select(v => v.ToString()));
---
+++ src/Confix.Tool/src/Confix.Tool/Utilities/Json/VariableIntellisenseRewriter.cs
@@ -0,0 +1,39 @@
+using System.Text.Json.Nodes;
+using Json.Schema;
+
+namespace Confix.Tool.Schema;
+
+public readonly record struct VariableIntellisenseContext(
+    JsonObject VariableReference
+);
+
+/// <summary>
+/// This rewriter is used to replace all primitives with a type allowing either the primitive
+/// or a variable. This is used to allow the user to specify a variable with the support
+/// of IntelliSense.
+/// </summary>
+public sealed class VariableIntellisenseRewriter : JsonDocumentRewriter<VariableIntellisenseContext>
+{
+    protected override JsonNode Rewrite(JsonObject obj, VariableIntellisenseContext context)
+    {
+        string? typeName = obj
+            .Select(x => x.Key)
+            .SingleOrDefault(x => x == TypeKeyword.Name);
+
+        return typeName switch
+        {
+            "string" or "number" or "integer" or "boolean" or "array" => RewritePrimitive(obj, context),
+            _ => base.Rewrite(obj, context),
+        };
+    }
+
+    private JsonObject RewritePrimitive(JsonObject obj, VariableIntellisenseContext context)
+        => new JsonObject()
+        {
+            [AnyOfKeyword.Name] = new JsonArray()
+                {
+                    base.Rewrite(obj, context),
+                    context.VariableReference,
+                }
+        };
+}
