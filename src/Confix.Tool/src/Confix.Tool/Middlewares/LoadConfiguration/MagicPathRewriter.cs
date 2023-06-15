c78a97448b2351bbaf21a9addd1a1dfad411c383
Florian Rohrer <dev@florianrohrer.ch>
2023-06-15 18:40:00 +0200

    implement MagicPathRewriter

--- src/Confix.Tool/src/Confix.Tool/Middlewares/LoadConfiguration/MagicPathRewriter.cs
+++ src/Confix.Tool/src/Confix.Tool/Middlewares/LoadConfiguration/MagicPathRewriter.cs
@@ -1,4 +1,6 @@
+using System.Text.Json.Nodes;
 using Confix.Tool.Schema;
+using Json.Schema;
 
 namespace Confix.Tool.Middlewares;
 
@@ -9,17 +11,78 @@ public record MagicPathContext(
     DirectoryInfo FileDirectory
 );
 
-/*
-    $home,
-    $repository
-    $project
-    ./ or .\
-*/
-
 /// <summary>
 ///     Rewrites the magic paths in the configuration files.
 /// </summary>
 public sealed class MagicPathRewriter : JsonDocumentRewriter<MagicPathContext>
 {
+    protected override JsonNode Rewrite(JsonValue value, MagicPathContext context)
+        => value.GetSchemaValueType() switch
+        {
+            SchemaValueType.String when value.ToMagicPath() is { } magicPath
+                => value.RewriteMagicPath(magicPath, context),
+            _ => base.Rewrite(value!, context)
+        };
+}
 
+file enum MagicPath
+{
+    Home,
+    Repository,
+    Project,
+    File
+}
+
+file static class MagicPathRewriterExtensions
+{
+    public static MagicPath? ToMagicPath(this JsonValue value)
+    {
+        if ((string?)value is { } stringValue)
+        {
+            return stringValue switch
+            {
+                string s when s.StartsWith("$home") => MagicPath.Home,
+                string s when s.StartsWith("$repository") => MagicPath.Repository,
+                string s when s.StartsWith("$project") => MagicPath.Project,
+                string s when s.StartsWith("./") || s.StartsWith(".\\") => MagicPath.File,
+                _ => null
+            };
+        }
+        return null;
+    }
+
+    public static JsonNode RewriteMagicPath(
+        this JsonValue value,
+        MagicPath magicPath,
+        MagicPathContext context)
+    {
+        var stringValue = ((string)value!).RemoveMagicPathPrefix(magicPath);
+        return JsonValue.Create(
+            magicPath switch
+            {
+                MagicPath.Home => Path.Combine(context.HomeDirectory.FullName, stringValue),
+
+                MagicPath.Repository when context.RepositoryDirectory is not null
+                    => Path.Combine(context.RepositoryDirectory.FullName, stringValue),
+
+                MagicPath.Project when context.ProjectDirectory is not null
+                    => Path.Combine(context.ProjectDirectory.FullName, stringValue),
+
+                MagicPath.File => Path.Combine(context.FileDirectory.FullName, stringValue),
+
+                _ => throw new ArgumentOutOfRangeException(
+                    nameof(magicPath),
+                    magicPath,
+                    "The magic path is not supported in this context")
+            });
+    }
+
+    private static string RemoveMagicPathPrefix(this string value, MagicPath magicPath)
+        => magicPath switch
+        {
+            MagicPath.Home => value.Remove(0, "$home".Length),
+            MagicPath.Repository => value.Remove(0, "$repository".Length),
+            MagicPath.Project => value.Remove(0, "$project".Length),
+            _ => value
+        };
 }
