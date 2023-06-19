ecee127bfd4fc0d2a3dd7eebbf52ef07fc3d1a54
Florian Rohrer <dev@florianrohrer.ch>
2023-06-19 16:10:01 +0200

    fix base path

--- docs/next.config.js
+++ docs/next.config.js
@@ -6,6 +6,7 @@ const withNextra = require("nextra")({
 module.exports = withNextra({
   output: "export",
   distDir: "out",
+  basePath: "/Confix-Reloaded",
   images: {
     unoptimized: true,
   },
