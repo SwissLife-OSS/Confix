b864b82f068a32ab32dda6daa7c32607c5c1d33a
Florian Rohrer <dev@florianrohrer.ch>
2023-06-19 15:44:44 +0200

    static export

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,12 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  },
+});
