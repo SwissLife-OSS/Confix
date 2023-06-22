e371caa3a0da48f833a2e529eb1caf9e864f2436
Florian Rohrer <dev@florianrohrer.ch>
2023-06-22 13:31:58 +0200

    Improve KeyVault ExitException handling (#68)
    Exit Exceptions now contain a Help message
    
    
    ![image](https://github.com/SwissLife-OSS/Confix/assets/20257550/927ff984-db01-40ff-a160-6e5ea12aa998)
    
    resolves #59
    
    ---------
    
    Co-authored-by: PascalSenn <senn.pasc@gmail.com>

--- src/Confix.Tool/src/Confix.Tool/Utilities/Console/Extensions/ExceptionHandlerCommandLineBuilderExtensions.cs
+++ src/Confix.Tool/src/Confix.Tool/Utilities/Console/Extensions/ExceptionHandlerCommandLineBuilderExtensions.cs
@@ -1,6 +1,8 @@
 using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
+using Confix.Tool.Commands.Logging;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Spectre.Console;
 
 namespace Confix.Tool;
@@ -22,11 +24,31 @@ public static class ExceptionHandlerCommandLineBuilderExtensions
         {
             await next(context);
         }
+        catch (AggregateException exception) when (exception.InnerExceptions.Any(e => e is ExitException))
+        {
+            context.ExitCode = ExitCodes.Error;
+
+            var logger = context.BindingContext.GetRequiredService<IConsoleLogger>();
+            var console = context.BindingContext.GetRequiredService<IAnsiConsole>();
+            foreach (var innerException in exception.InnerExceptions)
+            {
+                if (innerException is ExitException exitException)
+                {
+                    logger.ExitException(exitException);
+                    console.PrintHelp(exitException);
+                }
+                else
+                {
+                    logger.UnhandledException(innerException);
+                }
+            }
+        }
         catch (ExitException exception) when (exception is { Message: var message })
         {
             context.ExitCode = ExitCodes.Error;
 
-            context.BindingContext.GetRequiredService<IAnsiConsole>().Error(message);
+            context.BindingContext.GetRequiredService<IConsoleLogger>().ExitException(exception);
+            context.BindingContext.GetRequiredService<IAnsiConsole>().PrintHelp(exception);
         }
         catch (Exception ex) when (ex is OperationCanceledException or TaskCanceledException)
         {
@@ -34,8 +56,35 @@ public static class ExceptionHandlerCommandLineBuilderExtensions
         catch (Exception exception)
         {
             context.ExitCode = ExitCodes.Error;
-            context.BindingContext.GetRequiredService<IAnsiConsole>().WriteException(exception);
+            App.Log.UnhandledException(exception);
             throw;
         }
+    }
+}
+
+file static class LogExtensions
+{
+    public static void ExitException(this IConsoleLogger logger, ExitException exception)
+    {
+        logger.Error("Confix failed.");
+        logger.Information($"[red]{exception.Message}[/]");
+        logger.TraceException(exception);
+    }
+
+    public static void PrintHelp(this IAnsiConsole console, ExitException exception)
+    {
+        if (exception.Help is not null)
+        {
+            var panel = new Panel(exception.Help)
+            {
+                Header = new PanelHeader($"{Emoji.Known.LightBulb} Help", Justify.Left),
+            };
+            console.Write(panel);
+        }
+    }
+
+    public static void UnhandledException(this IConsoleLogger logger, Exception exception)
+    {
+        logger.Exception("Confix terminated unexpectedly.", exception);
     }
 }
