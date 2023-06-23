313a1efce85d654807e6bf652ee0953e1f1d7c81
Pascal Senn <senn.pasc@gmail.com>
2023-06-23 18:08:05 +0200

    Skip Schema Requirement for Component-Only Projects

--- src/Confix.Tool/src/Confix.Tool/Pipelines/Project/ProjectBuildPipeline.cs
+++ src/Confix.Tool/src/Confix.Tool/Pipelines/Project/ProjectBuildPipeline.cs
@@ -1,3 +1,4 @@
+using Confix.Tool.Commands.Logging;
 using Confix.Tool.Common.Pipelines;
 using Confix.Tool.Middlewares;
 using Confix.Tool.Middlewares.Project;
@@ -14,9 +15,43 @@ public sealed class ProjectBuildPipeline : Pipeline
             .UseReadConfigurationFiles()
             .UseEnvironment()
             .Use<BuildComponentsOfProjectMiddleware>()
+            .UseCompleteWhenNoConfigurationFiles()
             .Use<VariableMiddleware>()
             .Use<BuildProjectMiddleware>()
             .Use<ValidationMiddleware>()
             .UseWriteConfigurationFiles();
+    }
+}
+
+file static class Extensions
+{
+    public static IPipelineDescriptor UseCompleteWhenNoConfigurationFiles(
+        this IPipelineDescriptor builder)
+    {
+        builder.Use(Middleware);
+
+        return builder;
+    }
+
+    private static Task Middleware(IMiddlewareContext context, MiddlewareDelegate next)
+    {
+        var files = context.Features.Get<ConfigurationFileFeature>().Files;
+
+        if (files.Count == 0)
+        {
+            context.Logger.SkippedProjectBuildBecauseNoConfigurationFilesWereFound();
+            return Task.CompletedTask;
+        }
+
+        return next(context);
+    }
+}
+
+file static class Logs
+{
+    public static void SkippedProjectBuildBecauseNoConfigurationFilesWereFound(
+        this IConsoleLogger logger)
+    {
+        logger.Debug("Skipped project build because no configuration files were found");
     }
 }
