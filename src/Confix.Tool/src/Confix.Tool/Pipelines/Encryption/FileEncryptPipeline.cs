cfaae0ba8c0033132bc8094f7980a917f1cedc92
Florian Rohrer <dev@florianrohrer.ch>
2023-07-03 15:55:02 +0200

    Implement Encrypt Pipeline

--- src/Confix.Tool/src/Confix.Tool/Pipelines/Encryption/FileEncryptPipeline.cs
+++ src/Confix.Tool/src/Confix.Tool/Pipelines/Encryption/FileEncryptPipeline.cs
@@ -2,6 +2,7 @@ using Confix.Tool.Commands.Logging;
 using Confix.Tool.Common.Pipelines;
 using Confix.Tool.Middlewares;
 using Confix.Tool.Middlewares.Encryption;
+using Confix.Tool.Schema;
 
 namespace Confix.Tool.Pipelines.Encryption;
 
@@ -14,6 +15,8 @@ public sealed class FileEncryptPipeline : Pipeline
             .Use<LoadConfigurationMiddleware>()
             .UseEnvironment()
             .Use<EncryptionMiddleware>()
+            .AddArgument(InputFileArgument.Instance)
+            .AddArgument(OutputFileArgument.Instance)
             .UseHandler(InvokeAsync);
     }
 
@@ -21,8 +24,53 @@ public sealed class FileEncryptPipeline : Pipeline
     {
         context.SetStatus("Encrypting file...");
 
-        Thread.Sleep(5000);
+        FileInfo inputFile = context.Parameter.Get(InputFileArgument.Instance);
+        context.Parameter.TryGet(OutputFileArgument.Instance, out FileInfo? outputFile);
+        var encryptionFeature = context.Features.Get<EncryptionFeature>();
 
-        context.Logger.Success("Hello World!");
+        if (!inputFile.Exists)
+        {
+            context.Logger.FileNotExist(inputFile);
+            return;
+        }
+
+        context.Logger.ReadingFile(inputFile);
+        byte[] inputData = await inputFile.ReadAllBytes(context.CancellationToken);
+
+        context.Logger.Encrypting(inputData.Length);
+        byte[] encryptedData = await encryptionFeature.EncryptionProvider.EncryptAsync(inputData, context.CancellationToken);
+        context.Logger.Encrypted();
+
+        context.Logger.WritingFile(outputFile ?? inputFile);
+        await using Stream outputStream = (outputFile ?? inputFile).OpenReplacementStream();
+        await outputStream.WriteAsync(encryptedData);
+    }
+}
+
+file static class LogExtensions
+{
+    public static void FileNotExist(this IConsoleLogger console, FileInfo file)
+    {
+        console.Error($"File {file.ToLink()} does not exist.");
+    }
+
+    public static void ReadingFile(this IConsoleLogger console, FileInfo file)
+    {
+        console.Debug($"Reading {file.ToLink()}");
+    }
+
+    public static void Encrypting(this IConsoleLogger console, int length)
+    {
+        console.Debug($"Encrypting {length} bytes");
+    }
+
+    public static void Encrypted(this IConsoleLogger console)
+    {
+        console.Success("Encrypted successfully");
+    }
+
+    public static void WritingFile(this IConsoleLogger console, FileInfo file)
+    {
+        console.Information($"Writing data to {file.ToLink()}");
     }
 }
