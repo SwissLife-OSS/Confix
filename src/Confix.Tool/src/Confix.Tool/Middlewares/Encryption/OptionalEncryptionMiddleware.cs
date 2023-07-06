311b3cd3de63910b36b398facca69d26213feb26
Florian Rohrer <dev@florianrohrer.ch>
2023-07-06 09:24:30 +0200

    OptionalEncryptionMiddlewareTests

--- src/Confix.Tool/src/Confix.Tool/Middlewares/Encryption/OptionalEncryptionMiddleware.cs
+++ src/Confix.Tool/src/Confix.Tool/Middlewares/Encryption/OptionalEncryptionMiddleware.cs
@@ -13,35 +13,22 @@ public class OptionalEncryptionMiddleware : IMiddleware
 
     public virtual Task InvokeAsync(IMiddlewareContext context, MiddlewareDelegate next)
     {
-        ConfigurationFeature configurationFeature = context.Features.Get<ConfigurationFeature>();
-        if (configurationFeature.Encryption is not null)
+        var configurationFeature = context.Features.Get<ConfigurationFeature>();
+        if (configurationFeature.Encryption is { } encryptionConfiguration)
         {
-            EnvironmentFeature environmentFeature = context.Features.Get<EnvironmentFeature>();
-            string environmentName = environmentFeature.ActiveEnvironment.Name;
+            var environmentFeature = context.Features.Get<EnvironmentFeature>();
+            var environmentName = environmentFeature.ActiveEnvironment.Name;
 
-            var configuration = GetProviderConfiguration(configurationFeature, environmentName);
+            var configuration = new EncryptionProviderConfiguration
+            {
+                Type = encryptionConfiguration.Provider.Type,
+                Configuration = encryptionConfiguration.Provider.ValueWithOverrides(environmentName)
+            };
             var provider = _encryptionProviderFactory.CreateProvider(configuration);
 
             EncryptionFeature feature = new(provider);
             context.Features.Set(feature);
         }
         return next(context);
-    }
-
-    private static EncryptionProviderConfiguration GetProviderConfiguration(
-        ConfigurationFeature configurationFeature,
-        string environmentName)
-    {
-        if (configurationFeature.Encryption is null)
-        {
-            throw new ExitException("Encryption Provider not set");
-        }
-
-        var providerDefinition = configurationFeature.Encryption.Provider;
-        return new EncryptionProviderConfiguration
-        {
-            Type = providerDefinition.Type,
-            Configuration = providerDefinition.ValueWithOverrides(environmentName)
-        };
     }
 }
---
+++ src/Confix.Tool/test/Confix.Tool.Tests/Middlewares/Encryption/OptionalEncryptionMiddlewareTests.cs
@@ -0,0 +1,88 @@
+using System.Text.Json.Nodes;
+using Confix.Tool.Common.Pipelines;
+using Confix.Tool.Middlewares;
+using Confix.Tool.Middlewares.Encryption;
+using FluentAssertions;
+using Moq;
+
+namespace Confix.Tool.Tests.Middlewares.Encryption;
+
+public class OptionalEncryptionMiddlewareTests
+{
+    [Fact]
+    public void InvokeAsync_EncryptionConfigurationNull_DoesNotSetEncryptionFeature()
+    {
+        // Arrange
+        var contextMock = new Mock<IMiddlewareContext>(MockBehavior.Strict);
+
+        var featureCollection = new FeatureCollection();
+        ConfigurationFeature configurationFeature = new(
+           ConfigurationScope.None,
+           Mock.Of<IConfigurationFileCollection>(),
+           null,
+           null,
+           null,
+           null
+       );
+        featureCollection.Set(configurationFeature);
+        contextMock.SetupGet(x => x.Features).Returns(featureCollection);
+
+        var encryptionProviderFactory = new Mock<IEncryptionProviderFactory>(MockBehavior.Strict);
+        var middleware = new OptionalEncryptionMiddleware(encryptionProviderFactory.Object);
+        bool isNextInvoked = false;
+        MiddlewareDelegate next = _ =>
+        {
+            isNextInvoked = true;
+            return Task.CompletedTask;
+        };
+
+        // Act
+        middleware.InvokeAsync(contextMock.Object, next).Wait();
+
+        // Assert
+        featureCollection.TryGet<EncryptionFeature>(out _).Should().BeFalse();
+        isNextInvoked.Should().BeTrue();
+    }
+
+    [Fact]
+    public void InvokeAsync_EncryptionConfigurationNotNull_SetsEncryptionFeature()
+    {
+        // Arrange
+        var featureCollection = new FeatureCollection();
+        ConfigurationFeature configurationFeature = new(
+           ConfigurationScope.None,
+           Mock.Of<IConfigurationFileCollection>(),
+           null,
+           null,
+           null,
+           new(new("testProvider", new Dictionary<string, JsonObject>(), new()))
+       );
+        featureCollection.Set(configurationFeature);
+
+        var environmentFeature = new EnvironmentFeature(new("test", true));
+        featureCollection.Set(environmentFeature);
+
+        var contextMock = new Mock<IMiddlewareContext>(MockBehavior.Strict);
+        contextMock.SetupGet(x => x.Features).Returns(featureCollection);
+
+        var encryptionProviderFactory = new Mock<IEncryptionProviderFactory>(MockBehavior.Strict);
+        encryptionProviderFactory
+            .Setup(x => x.CreateProvider(It.IsAny<EncryptionProviderConfiguration>()))
+            .Returns(Mock.Of<IEncryptionProvider>());
+
+        var middleware = new OptionalEncryptionMiddleware(encryptionProviderFactory.Object);
+        bool isNextInvoked = false;
+        MiddlewareDelegate next = _ =>
+        {
+            isNextInvoked = true;
+            return Task.CompletedTask;
+        };
+
+        // Act
+        middleware.InvokeAsync(contextMock.Object, next).Wait();
+
+        // Assert
+        featureCollection.TryGet<EncryptionFeature>(out _).Should().BeTrue();
+        isNextInvoked.Should().BeTrue();
+    }
+}
