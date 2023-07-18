1584646a536520bd2d6290e494daf47d6460cc10
Pascal Senn <senn.pasc@gmail.com>
2023-07-18 15:19:06 +0200

    Local Variable provider improvements

--- src/Confix.Tool/src/Confix.Tool/Variables/Providers/Local/LocalVariableProvider.cs
+++ src/Confix.Tool/src/Confix.Tool/Variables/Providers/Local/LocalVariableProvider.cs
@@ -1,5 +1,8 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using Confix.Tool.Commands.Logging;
+using Confix.Tool.Schema;
+using HotChocolate.Types;
 using Json.More;
 
 namespace ConfiX.Variables;
@@ -7,18 +10,22 @@ namespace ConfiX.Variables;
 public sealed class LocalVariableProvider : IVariableProvider
 {
     private readonly Lazy<Dictionary<string, JsonNode?>> _parsedLocalFile;
+    private readonly FileInfo _localFile;
 
     public LocalVariableProvider(JsonNode configuration)
         : this(LocalVariableProviderConfiguration.Parse(configuration))
-    { }
+    {
+    }
 
     public LocalVariableProvider(LocalVariableProviderConfiguration configuration)
         : this(LocalVariableProviderDefinition.From(configuration))
-    { }
+    {
+    }
 
     public LocalVariableProvider(LocalVariableProviderDefinition definition)
-    { 
-         _parsedLocalFile = new(() => ParseConfiguration(definition));
+    {
+        _localFile = new FileInfo(definition.Path);
+        _parsedLocalFile = new(ParseConfiguration);
     }
 
     public Task<IReadOnlyList<string>> ListAsync(CancellationToken cancellationToken)
@@ -26,7 +33,16 @@ public sealed class LocalVariableProvider : IVariableProvider
 
     public Task<JsonNode> ResolveAsync(string path, CancellationToken cancellationToken)
     {
-        if (_parsedLocalFile.Value.TryGetValue(path, out JsonNode? value) && value is not null)
+        if (!_localFile.Exists)
+        {
+            // The resolving of the variable will fail, but the file will be created. The user can
+            // then edit the file. This is just a convenience for the user. Otherwise they have to
+            // create the file themselves.
+            App.Log.CreatingFile(_localFile);
+            File.WriteAllText(_localFile.FullName, "{}");
+        }
+
+        if (_parsedLocalFile.Value.TryGetValue(path, out var value) && value is not null)
         {
             return Task.FromResult(value.Copy()!);
         }
@@ -44,15 +60,37 @@ public sealed class LocalVariableProvider : IVariableProvider
         throw new NotImplementedException();
     }
 
-    private static Dictionary<string, JsonNode?> ParseConfiguration(LocalVariableProviderDefinition config)
+    private Dictionary<string, JsonNode?> ParseConfiguration()
     {
-        using FileStream fileStream = File.OpenRead(config.Path);
-        JsonNode node = JsonNode.Parse(fileStream) ?? throw new JsonException("Invalid Json Node");
+        if (!_localFile.Exists)
+        {
+            // If the file does not exist we just return an empty dictionary. This is needed
+            // for the case when the user does not have a local configuration file. 
+            App.Log.ConfigFileNotFound(_localFile);
+            return new Dictionary<string, JsonNode?>();
+        }
+
+        using var fileStream = _localFile.OpenRead();
+        var node = JsonNode.Parse(fileStream) ?? throw new JsonException("Invalid Json Node");
         return JsonParser.ParseNode(node);
     }
 
     public ValueTask DisposeAsync()
     {
         return ValueTask.CompletedTask;
+    }
+}
+
+file static class Log
+{
+    public static void CreatingFile(this IConsoleLogger logger, FileSystemInfo info)
+    {
+        logger.Warning(
+            $"The local variable file was not found at the expected location. Created empty file at: {info.ToLink()}");
+    }
+
+    public static void ConfigFileNotFound(this IConsoleLogger logger, FileSystemInfo info)
+    {
+        logger.Debug($"Local variable file was not found at expected location: {info.ToLink()}");
     }
 }
