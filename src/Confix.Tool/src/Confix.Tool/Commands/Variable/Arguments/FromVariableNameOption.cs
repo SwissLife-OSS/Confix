3cdca05f70151732df026ffc2f1c1b7d23c4f5d8
PascalSenn <senn.pasc@gmail.com>
2023-07-28 16:42:34 +0200

    Update FromVariableNameOption.cs

--- src/Confix.Tool/src/Confix.Tool/Commands/Variable/Arguments/FromVariableNameOption.cs
+++ src/Confix.Tool/src/Confix.Tool/Commands/Variable/Arguments/FromVariableNameOption.cs
@@ -2,7 +2,7 @@ using System.CommandLine;
 
 namespace Confix.Tool.Commands.Variable;
 
-internal sealed class FromVariableNameOption : Option<string>
+public sealed class FromVariableNameOption : Option<string>
 {
     public static FromVariableNameOption Instance { get; } = new();
 
