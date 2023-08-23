9b0e4d8f8b7455a0bcfa72ace125e51904b94910
Pascal Senn <senn.pasc@gmail.com>
2023-08-23 11:07:56 +0200

    Upgrade snapshots

--- src/Confix.Tool/test/Confix.Tool.Tests/Commands/Component/ComponentListCommandTests.cs
+++ src/Confix.Tool/test/Confix.Tool.Tests/Commands/Component/ComponentListCommandTests.cs
@@ -50,7 +50,7 @@ public class ComponentListCommandTests
             .AddOutput(cli)
             .AddReplacement(cli.Directories.Home.FullName, "HOME")
             .AddReplacement(cli.Directories.Content.FullName, "CONTENT")
-            .RemoveLineThatStartsWith("Parsing component from resource")
+            .RemoveLineThatStartsWith("  Parsing component from resource")
             .MatchSnapshot();
     }
 
