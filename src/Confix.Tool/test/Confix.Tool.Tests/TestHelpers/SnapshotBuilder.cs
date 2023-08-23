0edf466c3fc8150ae4a0684ad92f8e9909bb5a8c
Pascal Senn <senn.pasc@gmail.com>
2023-08-23 10:52:34 +0200

    Upgrade snapshots

--- src/Confix.Tool/test/Confix.Tool.Tests/TestHelpers/SnapshotBuilder.cs
+++ src/Confix.Tool/test/Confix.Tool.Tests/TestHelpers/SnapshotBuilder.cs
@@ -7,6 +7,7 @@ public sealed class SnapshotBuilder
 {
     private readonly StringBuilder _builder = new();
     private readonly List<(string, string)> _replacements = new();
+    private List<Func<string, string>> _processors = new();
 
     public SnapshotBuilder Append(string name, string content)
     {
@@ -20,7 +21,7 @@ public sealed class SnapshotBuilder
 
     public SnapshotBuilder AddReplacement(string original, string replacement)
     {
-        _replacements.Add((original, replacement));
+        _processors.Add(x => x.Replace(original, replacement));
         return this;
     }
 
@@ -32,12 +33,19 @@ public sealed class SnapshotBuilder
     public void MatchSnapshot()
     {
         var content = _builder.ToString();
-        foreach (var (original, replacement) in _replacements)
-        {
-            content = content.Replace(original, replacement);
-        }
+        content = _processors
+            .Aggregate(content, (current, processor) => processor(current));
 
         content.MatchSnapshot();
+    }
+
+    public SnapshotBuilder RemoveLineThatStartsWith(string value)
+    {
+        _processors.Add(
+            x => x.Split(Environment.NewLine)
+                .Where(y => !y.StartsWith(value))
+                .Aggregate((a, b) => a + Environment.NewLine + b));
+        return this;
     }
 
     public static SnapshotBuilder New() => new();
