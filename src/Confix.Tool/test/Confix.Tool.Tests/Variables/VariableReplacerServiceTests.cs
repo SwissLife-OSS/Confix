549b926355af589e5809a2125e1912e49661749c
Florian Rohrer <dev@florianrohrer.ch>
2023-12-04 15:56:21 +0100

    prove with test that nesting is working

--- src/Confix.Tool/test/Confix.Tool.Tests/Variables/VariableReplacerServiceTests.cs
+++ src/Confix.Tool/test/Confix.Tool.Tests/Variables/VariableReplacerServiceTests.cs
@@ -45,7 +45,7 @@ public class VariableReplacerServiceTests
     }
 
     [Fact]
-    public async Task RewriteAsync_JsonValue_()
+    public async Task RewriteAsync_JsonValue_ResolvesCorrectly()
     {
         // arrange
         JsonNode node = JsonValue.Create("$test:variable.number")!;
@@ -70,5 +70,99 @@ public class VariableReplacerServiceTests
 
         // assert
         Assert.Equal("Replaced", result?.ToString());
+    }
+
+    [Fact]
+    public async Task RewriteAsync_VariablesInNestedArray_ResolvesCorrectly()
+    {
+        // arrange
+        JsonNode node = JsonNode.Parse("""
+            {
+                "foo": "$test:variable.array"
+            }
+        """)!;
+
+        Mock<IVariableResolver> variableResolverMock = new(MockBehavior.Strict);
+        variableResolverMock.Setup(x => x.ResolveVariables(
+                It.IsAny<IReadOnlyList<VariablePath>>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync((IReadOnlyList<VariablePath> keys, CancellationToken _) =>
+            {
+                var result = new Dictionary<VariablePath, JsonNode>();
+                foreach (var key in keys)
+                {
+                    if (key.Path == "variable.array")
+                    {
+                        result[key] = JsonNode.Parse("""
+                            [
+                                "notAVariable",
+                                "$test:variable.string_nested"
+                            ]
+                        """)!;
+                    }
+                    else if(key.Path == "variable.string_nested"){
+                        result[key] = JsonValue.Create("$test:variable.string")!;
+                    }
+                    else
+                    {
+                        result[key] = JsonValue.Create("Replaced Value of " + key.Path)!;
+                    }
+                }
+                return result;
+            });
+        VariableReplacerService service = new(variableResolverMock.Object);
+
+        // act
+        var result = await service.RewriteAsync(node, default);
+
+        // assert
+        result?.ToString().MatchSnapshot();
+    }
+
+    [Fact]
+    public async Task RewriteAsync_VariablesInNestedObject_ResolvesCorrectly()
+    {
+        // arrange
+        JsonNode node = JsonNode.Parse("""
+            {
+                "foo": "$test:variable.someObject"
+            }
+        """)!;
+
+        Mock<IVariableResolver> variableResolverMock = new(MockBehavior.Strict);
+        variableResolverMock.Setup(x => x.ResolveVariables(
+                It.IsAny<IReadOnlyList<VariablePath>>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync((IReadOnlyList<VariablePath> keys, CancellationToken _) =>
+            {
+                var result = new Dictionary<VariablePath, JsonNode>();
+                foreach (var key in keys)
+                {
+                    if (key.Path == "variable.someObject")
+                    {
+                        result[key] = JsonNode.Parse("""
+                            {
+                                "notAVariable": "notAVariable",
+                                "string_nested": "$test:variable.string_nested"
+                            }
+                        """)!;
+                    }
+                    else if(key.Path == "variable.string_nested"){
+                        result[key] = JsonValue.Create("$test:variable.string")!;
+                    }
+                    else
+                    {
+                        result[key] = JsonValue.Create("Replaced Value of " + key.Path)!;
+                    }
+                }
+                return result;
+            });
+        VariableReplacerService service = new(variableResolverMock.Object);
+
+        // act
+        var result = await service.RewriteAsync(node, default);
+
+        // assert
+        result?.ToString().MatchSnapshot();
     }
 }
