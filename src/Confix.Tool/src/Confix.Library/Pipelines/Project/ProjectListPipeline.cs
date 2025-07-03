ec91284e2736590062369fe544354f90743f3009
glucaci <gabriel.lucaci.dev@gmail.com>
2025-07-03 09:11:12 +0200

    Remove unused middleware and simplify project file search logic

--- src/Confix.Tool/src/Confix.Library/Pipelines/Project/ProjectListPipeline.cs
+++ src/Confix.Tool/src/Confix.Library/Pipelines/Project/ProjectListPipeline.cs
@@ -1,7 +1,6 @@
 using Confix.Tool.Commands.Logging;
 using Confix.Tool.Commands.Temp;
 using Confix.Tool.Common.Pipelines;
-using Confix.Tool.Middlewares;
 using Confix.Tool.Schema;
 
 namespace Confix.Tool.Commands.Project;
@@ -12,17 +11,13 @@ public sealed class ProjectListPipeline : Pipeline
     protected override void Configure(IPipelineDescriptor builder)
     {
         builder
-            .Use<LoadConfigurationMiddleware>()
             .AddOption(DirectoriesOnlyOption.Instance)
             .UseHandler(InvokeAsync);
     }
 
     private static Task InvokeAsync(IMiddlewareContext context)
     {
-        context.SetStatus("Finding all project files...");
-
-        var configuration = context.Features.Get<ConfigurationFeature>();
-        var searchDirectory = configuration.Solution?.Directory ?? context.Execution.CurrentDirectory;
+        var searchDirectory = context.Execution.CurrentDirectory;
         var projectFiles = searchDirectory.FindAllInPath(FileNames.ConfixProject);
         var directoriesOnly = context.Parameter.TryGet(DirectoriesOnlyOption.Instance, out bool dirs) && dirs;
 
